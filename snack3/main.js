4cfb19971346f4193d078675b0e5aebd832bfe63
giogiosgr <giovannisgr88@gmail.com>
2024-05-23 19:43:57 +0200

    feat: versione con utilizzo del metodo filter

--- snack3/main.js
+++ snack3/main.js
@@ -11,18 +11,25 @@ const animals =
         { nome: 'topo', famiglia: 'muridi', classe: 'mammiferi' },
     ];
 
+/* LOGICA ORIGINALE
+
 //inizializziamo un nuovo array, come vuoto, per contenere successivamente la lista dei mammiferi
 const mammals = [];
 
 //ciclo for per iterare ogni elemento dell'array
 for (let i = 0; i < animals.length; i++) {
-    //condizione in cui valutiamo l'oggetto corrispondente all'indice attuale
-    //accediamo alla key classe dell'oggetto utilizzando la dot notation
-    if (animals[i].classe === 'mammiferi') {
-        //se ha mammifero come classe, pushiamo l'oggetto per l'animale corrente nell'array dedicato
-        mammals.push(animals[i])
-    }
+//condizione in cui valutiamo l'oggetto corrispondente all'indice attuale
+//accediamo alla key classe dell'oggetto utilizzando la dot notation
+if (animals[i].classe === 'mammiferi') {
+    //se ha mammifero come classe, pushiamo l'oggetto per l'animale corrente nell'array dedicato
+    mammals.push(animals[i])
 }
+}
+*/
+
+/*Logica post lezione sui metodi degli array */
+//inizializziamo il nuovo array assegnandogli il risultato del metodo filter (con arrow function) sull'array originale
+const mammals = animals.filter((animal) => animal.classe === 'mammiferi');
 
 //stampiamo in console l'array risultato
 console.log(mammals);
